530cabd008f18085c16089e0448e046ee3a323c8
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2023-11-27 11:04:32 +0200

    Update and rename glowing_entities.java to glowing_entities_mixin.java

--- src/main/java/net/i_no_am/glowing_entities/mixin/glowing_entities_mixin.java
+++ src/main/java/net/i_no_am/glowing_entities/mixin/glowing_entities_mixin.java
@@ -1,6 +1,5 @@
-package net.i_no_am.glowing_entities.mixins;
+package net.i_no_am.glowing_entities.mixin;
 
-import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,8 +7,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(glowing_entities.class)
-public abstract class glowing_entities {
+@Mixin(glowing_entities_mixin.class)
+public abstract class glowing_entities_mixin {
 
     @Inject(method = "getBlockLight", at = @At("RETURN"), cancellable = true)
     public <T extends Entity> void getLight(T entity, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
