0806e70a4aaa840bd44ea8b7e7dd9fa1a8f019a9
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2023-11-27 10:51:51 +0200

    Update glowing_entities.java

--- src/main/java/net/i_no_am/glowing_entities/mixin/glowing_entities.java
+++ src/main/java/net/i_no_am/glowing_entities/mixin/glowing_entities.java
@@ -11,7 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(glowing_entities.class)
 public abstract class glowing_entities {
 
-@Inject(method = "getBlockLight", at = @At("RETURN"), cancellable = true)
-public <T extends Entity> void getLight(T entity, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-    cir.setReturnValue(15);
+    @Inject(method = "getBlockLight", at = @At("RETURN"), cancellable = true)
+    public <T extends Entity> void getLight(T entity, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(15);
+    }
 }
