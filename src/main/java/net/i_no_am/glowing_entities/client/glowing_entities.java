29633012a1802cc62e64cb2adac4ebfa74432310
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2023-11-27 11:28:47 +0200

    Update and rename glowing_entities_check.java to glowing_entities.java

--- src/main/java/net/i_no_am/glowing_entities/client/glowing_entities.java
+++ src/main/java/net/i_no_am/glowing_entities/client/glowing_entities.java
@@ -7,8 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
-public class glowing_entities_check implements ClientModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("glowing_entities_check");
+public class glowing_entities implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("glowing_entities");
 
 	@Override
 	public void onInitializeClient() {
