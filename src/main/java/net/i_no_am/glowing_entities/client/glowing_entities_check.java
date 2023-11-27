a31df31736453002c47ba81109f59f96e6309030
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2023-11-27 10:45:54 +0200

    Update and rename autototem.java to glowing_entities_check.java

--- src/main/java/net/i_no_am/glowing_entities/client/glowing_entities_check.java
+++ src/main/java/net/i_no_am/glowing_entities/client/glowing_entities_check.java
@@ -7,8 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
-public class autototem implements ClientModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("AutoTotem");
+public class glowing_entities_check implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("glowing_entities_check");
 
 	@Override
 	public void onInitializeClient() {
