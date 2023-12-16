815cc3cfd7d8aec61634d43f66098287a68563b8
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2023-12-16 22:42:14 +0200

    Update glowing_entities_mixin.java

--- src/main/java/net/i_no_am/glowing_entities/mixin/glowing_entities_mixin.java
+++ src/main/java/net/i_no_am/glowing_entities/mixin/glowing_entities_mixin.java
@@ -1,5 +1,6 @@
 package net.i_no_am.glowing_entities.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.util.math.BlockPos;
@@ -9,10 +10,33 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityRenderer.class)
-public abstract class glowing_entities_mixin {
+public abstract class GlowingEntitiesMixin {
+
+    private static boolean glowingEnabled = true; // Default to true
+    private static int customLightLevel = 15; // Default light level
 
     @Inject(method = "getBlockLight", at = @At("RETURN"), cancellable = true)
     public <T extends Entity> void getLight(T entity, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(15);
+        if (glowingEnabled) {
+            cir.setReturnValue(customLightLevel);
+        }
+    }
+
+    // Command to toggle glowing state
+    public static void toggleGlowing() {
+        glowingEnabled = !glowingEnabled;
+        MinecraftClient.getInstance().player.sendMessage(
+                glowingEnabled ? "Glowing is now enabled." : "Glowing is now disabled."
+        );
+    }
+
+    // Command to set custom light level
+    public static void setCustomLightLevel(String level) {
+        try {
+            customLightLevel = Integer.parseInt(level);
+            MinecraftClient.getInstance().player.sendMessage("Custom light level set to " + customLightLevel);
+        } catch (NumberFormatException e) {
+            MinecraftClient.getInstance().player.sendMessage("Invalid light level. Please provide a valid integer.");
+        }
     }
 }
